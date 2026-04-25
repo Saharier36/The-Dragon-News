acb04b27fe37189b969f7be1451039c77ac08b30
Saharier Omi <gsaharier761@gmail.com>
2026-04-25 21:17:54 +0600

    add Fonts

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import logo from '@/assets/logo.png';
-import Image from 'next/image';
-import { format } from 'date-fns';
+import React from "react";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import { format } from "date-fns";
 
 const Header = () => {
-    return (
-      <div className="text-center py-8 space-y-2">
-        <Image
-          className="mx-auto"
-          src={logo}
-          alt="Logo"
-          width={300}
-          height={200}
-        />
-        <p className="text-[#403F3F]">Journalism Without Fear or Favour</p>
-        <p className="text-[#403F3F] font-medium">{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
-      </div>
-    );
+  return (
+    <div className="text-center py-8 space-y-2">
+      <Image
+        className="mx-auto"
+        src={logo}
+        alt="Logo"
+        width={300}
+        height={200}
+      />
+      <p className="text-[#403F3F]">Journalism Without Fear or Favour</p>
+      <p className="text-[#403F3F] font-medium">
+        {format(new Date(), "EEEE, MMMM dd, yyyy")}
+      </p>
+    </div>
+  );
 };
 
 export default Header;

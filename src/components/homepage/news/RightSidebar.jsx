15b44e7fcf0a8ffebfbb2f15a631ae1bfe6203e0
Saharier Omi <gsaharier761@gmail.com>
2026-04-26 23:45:44 +0600

    Enhance RightSidebar with login options and social links; implement navigation for social icons

--- src/components/homepage/news/RightSidebar.jsx
+++ src/components/homepage/news/RightSidebar.jsx
@@ -12,6 +12,7 @@ import swimming from "@/assets/swimming.png";
 import classroom from "@/assets/class.png";
 import playground from "@/assets/playground.png";
 import background from "@/assets/bg.png";
+import Link from "next/link";
 
 const socials = [
   { icon: <FaFacebook className="text-blue-600 text-xl" />, label: "Facebook" },
@@ -31,34 +32,40 @@ const qZones = [
 const RightSidebar = () => {
   return (
     <div>
+      {/* LogeIn With */}
       <div>
         <h3 className="text-xl font-semibold mb-4">Login With</h3>
-        <button className="w-full mb-2 py-2 px-4 border border-blue-500 text-blue-500 rounded-md flex items-center justify-center gap-2 hover:bg-gray-100">
+        <button className="w-full mb-2 py-2 px-4 border border-blue-500 text-blue-500 rounded-md flex items-center justify-center gap-2 hover:bg-gray-100 cursor-pointer">
           <i className="fab fa-google text-red-500"></i>
           <FaGoogle />
           Login with Google
         </button>
 
-        <button className="w-full mb-2 py-2 px-4 border border-gray-500 rounded-md flex items-center justify-center gap-2 hover:bg-gray-100">
+        <button className="w-full mb-2 py-2 px-4 border border-gray-500 rounded-md flex items-center justify-center gap-2 hover:bg-gray-100 cursor-pointer">
           <i className="fab fa-github text-gray-800"></i>
           <FaGithub />
           Login with GitHub
         </button>
       </div>
+      {/* Find Us On */}
       <div className="mt-8">
         <h3 className="text-xl font-semibold mb-4">Find Us On</h3>
         <ul className="border border-gray-200 rounded-xl divide-y divide-gray-200">
           {socials.map(({ icon, label }) => (
-            <li key={label} className="flex items-center gap-3 px-4 py-3">
+            <Link
+              href={"find-us-on"}
+              key={label}
+              className="flex items-center gap-3 px-4 py-3 cursor-pointer"
+            >
               <span className="w-9 h-9 flex items-center justify-center rounded-full bg-[#F3F3F3]">
                 {icon}
               </span>
               <span className="text-gray-600 font-medium">{label}</span>
-            </li>
+            </Link>
           ))}
         </ul>
       </div>
-
+      {/* Q-Zone */}
       <div className="mt-8">
         <div className="flex flex-col gap-10 bg-[#F3F3F3] p-4">
           <h3 className="text-xl font-semibold mb-4">Q-Zone</h3>
@@ -69,6 +76,7 @@ const RightSidebar = () => {
           ))}
         </div>
       </div>
+      {/* bg */}
       <Image
         className="hidden md:block mt-8 rounded-md"
         src={background}

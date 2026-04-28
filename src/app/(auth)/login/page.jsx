25769fcdf925abb0724af61d03116abad0d82957
Saharier Omi <gsaharier761@gmail.com>
2026-04-28 21:16:36 +0600

    Enhance authentication forms with react-hook-form integration and validation

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,6 +1,19 @@
+"use client";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const handleLoginFunc = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="min-h-screen bg-base-200 flex items-center justify-center">
       <div className="card bg-base-100 shadow rounded w-105">
@@ -10,31 +23,41 @@ const LoginPage = () => {
           </h2>
           <div className="divider mb-5"></div>
 
-          <div className="form-control mb-3">
-            <label className="label pb-1">
-              <span className="label-text font-semibold">Email address</span>
-            </label>
-            <input
-              type="email"
-              placeholder="Enter your email address"
-              className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
-            />
-          </div>
+          <form onSubmit={handleSubmit(handleLoginFunc)}>
+            <div className="form-control mb-3">
+              <label className="label pb-1">
+                <span className="label-text font-semibold">Email address<span className="text-error">*</span></span>
+              </label>
+              <input
+                type="email"
+                placeholder="Enter your email address"
+                className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
+                {...register("email", { required: "Email is required" })}
+              />
+              {errors.email && <span className="text-error text-xs">{errors.email.message}</span>}
+            </div>
 
-          <div className="form-control mb-5">
-            <label className="label pb-1">
-              <span className="label-text font-semibold">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
-            />
-          </div>
+            <div className="form-control mb-5">
+              <label className="label pb-1">
+                <span className="label-text font-semibold">Password<span className="text-error">*</span></span>
+              </label>
+              <input
+                type="password"
+                placeholder="Enter your password"
+                className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
+                {...register("password", { required: "Password is required" })}
+              />
+              {errors.password && (
+                <span className="text-error text-xs">{errors.password.message}</span>
+              )}
+            </div>
 
-          <button className="btn bg-[#403F3F] text-white w-full">Login</button>
+            <button className="btn bg-[#403F3F] text-white w-full">
+              Login
+            </button>
+          </form>
 
-          <p className="text-center mt-3 text-sm text-[#706F6F]">
+          <p className="text-center mt-3 text-sm font-semibold text-[#706F6F]">
             Don&apos;t Have An Account ?{" "}
             <a href="/register" className="text-error font-semibold">
               Register
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,6 +1,18 @@
+"use client";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleRegisterFunc = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="min-h-screen bg-base-200 flex items-center justify-center">
       <div className="card bg-base-100 shadow rounded w-105">
@@ -10,58 +22,96 @@ const RegisterPage = () => {
           </h2>
           <div className="divider mb-5"></div>
 
-          <div className="form-control mb-3">
-            <label className="label pb-1">
-              <span className="label-text font-semibold">Your Name</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter your name"
-              className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
-            />
-          </div>
+          <form onSubmit={handleSubmit(handleRegisterFunc)}>
+            <div className="form-control mb-3">
+              <label className="label pb-1">
+                <span className="label-text font-semibold">
+                  Your Name<span className="text-error">*</span>
+                </span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your name"
+                className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
+                {...register("name", { required: "Name is required" })}
+              />
+              {errors.name && (
+                <span className="text-error text-xs">
+                  {errors.name.message}
+                </span>
+              )}
+            </div>
 
-          <div className="form-control mb-3">
-            <label className="label pb-1">
-              <span className="label-text font-semibold">Photo URL</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter your photo URL"
-              className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
-            />
-          </div>
+            <div className="form-control mb-3">
+              <label className="label pb-1">
+                <span className="label-text font-semibold">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your photo URL"
+                className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
+                {...register("photoUrl")}
+              />
+            </div>
 
-          <div className="form-control mb-3">
-            <label className="label pb-1">
-              <span className="label-text font-semibold">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="Enter your email address"
-              className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
-            />
-          </div>
+            <div className="form-control mb-3">
+              <label className="label pb-1">
+                <span className="label-text font-semibold">
+                  Email<span className="text-error">*</span>
+                </span>
+              </label>
+              <input
+                type="email"
+                placeholder="Enter your email address"
+                className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
+                {...register("email", { required: "Email is required" })}
+              />
+              {errors.email && (
+                <span className="text-error text-xs">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
 
-          <div className="form-control mb-3">
-            <label className="label pb-1">
-              <span className="label-text font-semibold">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
-            />
-          </div>
+            <div className="form-control mb-3">
+              <label className="label pb-1">
+                <span className="label-text font-semibold">
+                  Password<span className="text-error">*</span>
+                </span>
+              </label>
+              <input
+                type="password"
+                placeholder="Enter your password"
+                className="input input-bordered input-sm h-10 bg-base-200 focus:outline-none"
+                {...register("password", { required: "Password is required" })}
+              />
+              {errors.password && (
+                <span className="text-error text-xs">
+                  {errors.password.message}
+                </span>
+              )}
+            </div>
 
-          <div className="form-control mb-5">
-            <label className="label cursor-pointer justify-start gap-2">
-              <input type="checkbox" className="checkbox checkbox-sm" />
-              <span className="label-text">Accept Term & Conditions</span>
-            </label>
-          </div>
+            <div className="form-control mb-5">
+              <label className="label cursor-pointer justify-start gap-2">
+                <input
+                  type="checkbox"
+                  className="checkbox checkbox-sm"
+                  {...register("terms", { required: "You must accept terms" })}
+                />
+                <span className="label-text">Accept Term & Conditions</span>
+              </label><br />
+              {errors.terms && (
+                <span className="text-error text-xs">
+                  {errors.terms.message}
+                </span>
+              )}
+            </div>
 
-          <button className="btn bg-[#403F3F] text-white w-full">Register</button>
+            <button className="btn bg-[#403F3F] text-white w-full">
+              Register
+            </button>
+          </form>
         </div>
       </div>
     </div>
